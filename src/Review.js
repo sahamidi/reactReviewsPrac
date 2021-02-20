d6e949631203bbafb59b5e5a0b9adb3646fbcba8
Sal Hamidi <hamidisa87@gmail.com>
2021-02-19 21:45:06 -0500

    Set non dynamic review

--- src/Review.js
+++ src/Review.js
@@ -1,9 +1,17 @@
-import React, { useState } from 'react';
-import people from './data';
-import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
+import React, { useState } from 'react'
+import people from './data'
+import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa'
 
 const Review = () => {
-  return <h2>review component</h2>;
-};
+  let [index, setIndex] = useState(0)
+  let { name, job, image, text } = people[index]
+  return (
+    <article className='review'>
+      <div className='img-container'>
+        <img src={image} alt={name} className='person-img' />
+      </div>
+    </article>
+  )
+}
 
-export default Review;
+export default Review
